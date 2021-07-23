4e56b109766cd5cab22ac0ca82c17dad1161bee2
ziaurgolang <rahomanz203@gmail.com>
2021-07-23 12:15:01 +0600

    learn

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+/*
+var name string = "ziaur"
+
+var name2 , city string = "zahid" , "chittagong"
+
+fmt.Println(name , city , name2)
+*/
+var c rune = 'A'
+var age int = 5
+var result float32 = 3.94
+
+fmt.Println(c , age , result)
+
+
+}
